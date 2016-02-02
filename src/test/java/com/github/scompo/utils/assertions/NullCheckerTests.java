cd109d9d5a8dc2350ac80449df79f659778ca7cb
Mauro Scomparin <scompo@gmail.com>
2016-02-03 00:20:16 +0100

    implemented NullCheckerTests

---
+++ src/test/java/com/github/scompo/utils/assertions/NullCheckerTests.java
@@ -0,0 +1,30 @@
+package com.github.scompo.utils.assertions;
+
+import static com.github.scompo.utils.assertions.NullChecker.isNotNull;
+import static com.github.scompo.utils.assertions.NullChecker.isNull;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class NullCheckerTests {
+
+	private static final Object NULL_OBJ = null;
+
+	private static final Object NOT_NULL_OBJ = new Object();
+
+	@Test
+	public void testIsNull() {
+
+		assertTrue(isNull(NULL_OBJ));
+		assertFalse(isNull(NOT_NULL_OBJ));
+	}
+
+	@Test
+	public void testIsNotNull() {
+
+		assertFalse(isNotNull(NULL_OBJ));
+		assertTrue(isNotNull(NOT_NULL_OBJ));
+	}
+
+}

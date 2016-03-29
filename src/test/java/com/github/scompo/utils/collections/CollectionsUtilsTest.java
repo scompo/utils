0fed6b97c06834729a45cd81014fe97322cef206
Mauro Scomparin <scompo@gmail.com>
2016-03-30 01:20:13 +0200

    fixed issues from https://www.codacy.com/app/scompo/utils/file/2322849525/issues/source?bid=3107434&fileBranchId=3107434#l35

--- src/test/java/com/github/scompo/utils/collections/CollectionsUtilsTest.java
+++ src/test/java/com/github/scompo/utils/collections/CollectionsUtilsTest.java
@@ -2,6 +2,7 @@ package com.github.scompo.utils.collections;
 
 import static com.github.scompo.utils.collections.CollectionsUtils.getOnlyElement;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -14,47 +15,56 @@ import org.junit.rules.ExpectedException;
 
 public class CollectionsUtilsTest {
 
+	private static final String SHOULD_HAVE_THROWN_AN_ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE = "should have thrown an IllegalArgumentException";
+
 	private static final Long FIRST_ELEMENT = 1L;
+
 	private static final Long SECOND_ELEMENT = 2L;
 
 	private static final List<Long> COLLECTION_WITH_ONLY_ONE_ELEMENT = Arrays.asList(FIRST_ELEMENT);
-	private static final List<Long> COLLECTION_WITH_MORE_THAN_ONE_ELEMENT = Arrays.asList(FIRST_ELEMENT, SECOND_ELEMENT);
+
+	private static final List<Long> COLLECTION_WITH_MORE_THAN_ONE_ELEMENT = Arrays.asList(FIRST_ELEMENT,
+			SECOND_ELEMENT);
+
 	private static final Collection<Object> EMPTY_COLLECTION = Collections.emptyList();
+
 	private static final Collection<Object> NULL_COLLECTION = null;
-	
+
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
-	
+
 	@Test
 	public void getOnlyElementShouldReturnTheOnlyElementIfTheCollectionHasOnlyOneElement() {
 
 		assertEquals(FIRST_ELEMENT, getOnlyElement(COLLECTION_WITH_ONLY_ONE_ELEMENT));
 	}
-	
+
 	@Test
 	public void getOnlyElementShouldThrowIllegalArgumentExceptionIfTheCollectionHasMoreThanOneElement() {
-		
+
 		thrown.expect(IllegalArgumentException.class);
 		thrown.expectMessage("Expected 1 element, found: 2");
-		
-		getOnlyElement(COLLECTION_WITH_MORE_THAN_ONE_ELEMENT);
+
+		assertNull(SHOULD_HAVE_THROWN_AN_ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE,
+				getOnlyElement(COLLECTION_WITH_MORE_THAN_ONE_ELEMENT));
+
 	}
-	
+
 	@Test
 	public void getOnlyElementShouldThrowIllegalArgumentExceptionIfTheCollectionHasNoElements() {
-		
+
 		thrown.expect(IllegalArgumentException.class);
 		thrown.expectMessage("Expected 1 element, found: 0");
-		
-		getOnlyElement(EMPTY_COLLECTION);
+
+		assertNull(SHOULD_HAVE_THROWN_AN_ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE, getOnlyElement(EMPTY_COLLECTION));
 	}
-	
+
 	@Test
 	public void getOnlyElementShouldThrowNullPointerExceptionIfTheCollectionIsNull() {
-		
+
 		thrown.expect(NullPointerException.class);
-		
-		getOnlyElement(NULL_COLLECTION);
+
+		assertNull("should have thrown a NullPointerException", getOnlyElement(NULL_COLLECTION));
 	}
 
 }
